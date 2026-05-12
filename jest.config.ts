7c5757a90c0bcf0c0e1741da8f2abd7b85e675d0
owent <>
2026-05-12 10:16:20 +0800

    Upgrade deps

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,22 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
-// import { defaults } from "jest-config";
-import { jsWithTsESM } from "ts-jest/presets";
+import tsJestPresets from "ts-jest/presets/index.js";
 
 const config: JestConfigWithTsJest = {
-  ...jsWithTsESM,
+  ...tsJestPresets.jsWithTsESM,
   clearMocks: true,
   verbose: true,
+  transformIgnorePatterns: [
+    "node_modules/(?!.*(mime|globby|string-env-interpolation|micromatch)/)"
+  ],
+  transform: {
+    "^.+\\.m?[tj]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+        useESM: true,
+      },
+    ],
+  },
 };
 
 export default config;
